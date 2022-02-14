b76b9e182cec137fbb8ff55f77862d4483de97f1
Emilian Harastasan <eharastasan@gmail.com>
2022-02-14 23:48:39 +0100

    chord: delete tasks folder

--- tasks/example-with-lib.js
+++
@@ -1,9 +0,0 @@
-const { task } = require("@iboss/terrain");
-const lib = require("../lib");
-
-task(async (env) => {
-  const { getPosts, increment } = lib(env);
-  console.log("count 1 = ", await getPosts());
-  await increment();
-  console.log("count 2 = ", await getPosts());
-});
